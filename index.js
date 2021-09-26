e5c9d4f38ce36cc61097e48d10ae351122152c48
Rishabh Kunwar <rishabhkunwar11@gmail.com>
2021-09-26 16:07:54 +0530

    basic express server setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+const PORT = process.env.PORT || 8000;
+
+app.use('/' , (req , res) => {
+    res.json({success : true , message : 'this is an API for Sport Connect'})
+})
+
+
+app.use((req , res) => {
+    res.status(404).json({success : false  , message : 'page not found'})
+})
+
+app.use((err , req ,res , next) => {
+    console.error(err.stack)
+    res.status(500).json({success : false , message : 'something went wrong' , error : err.message})
+})
+app.listen(PORT , () => console.log('server started at port' , PORT))
