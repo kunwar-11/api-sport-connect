7c922fd478209004a48c9586b6567216ab7a4551
Rishabh Kunwar <rishabhkunwar11@gmail.com>
2021-09-29 15:57:11 +0530

    bug fixes in feed route

--- routes/feeds.route.js
+++ routes/feeds.route.js
@@ -1,9 +1,11 @@
 // to get the loggedin user feed -->  done
 const express = require("express");
+const { each } = require("lodash");
 const { verifyToken } = require("../middleware/verifytoken");
 const { Feed } = require("../models/feeds.model");
 const router = express.Router();
 const { Post } = require("../models/post.model");
+const { User } = require("../models/user.model");
 
 router.use(verifyToken);
 
@@ -12,7 +14,20 @@ router.route("/").get(async (req, res) => {
   try {
     let feeds = await Feed.findOne({ _id: userId });
     let posts = await Post.find({});
-    feeds.feeds = posts;
+    let { following } = await User.findById(userId);
+    console.log("before", posts);
+    console.log(following);
+    let Newposts = posts.map((item) => {
+      if (
+        following.some((each) => each._id.toString() == item.uid.toString())
+      ) {
+        return item;
+      }
+      return null;
+    });
+    Newposts = Newposts.filter((each) => each !== null);
+    console.log("after", Newposts);
+    feeds.feeds = Newposts;
     await feeds.save();
     feeds = await feeds.populate("feeds._id").execPopulate();
     const NormalizedFeed = feeds.feeds.map((each) => each._id._doc);
